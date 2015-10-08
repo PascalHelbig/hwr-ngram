18004776eef071d8c863fb2acfc393d2d30ab79f
pascalhelbig <pascalhelbig@gmail.com>
2015-10-08 09:07:02 +0200

    try to fix coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('nsp', function (cb) {
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib\**\*.js')
+  return gulp.src('lib/**.js')
     .pipe(istanbul({
       includeUntested: true
     }))
