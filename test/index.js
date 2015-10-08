73f5b270b12ce3497425827e72a63eae78a527a6
pascalhelbig <pascalhelbig@gmail.com>
2015-10-08 11:31:41 +0200

    added n & predictOnlyNext

--- test/index.js
+++ test/index.js
@@ -1,52 +1,77 @@
 'use strict';
 
 var assert = require('assert');
-var bigram = require('../lib');
+var ngram = require('../lib');
 
-describe('bigram', function () {
+describe('ngram', function () {
 
   it('should return empty arrays if nothing was learned.', function () {
-    assert.deepEqual(bigram.prediction(''), []);
-    assert.deepEqual(bigram.prediction('abc'), []);
+    ngram.clean();
+    ngram.n = 2;
+    assert.deepEqual(ngram.prediction(''), []);
+    assert.deepEqual(ngram.prediction('abc'), []);
   });
 
   it('should return every prediction for every word (except the last one)', function () {
-    bigram.learn('I love working with node');
-    assert.deepEqual(bigram.prediction('I'), ['love']);
-    assert.deepEqual(bigram.prediction('love'), ['working']);
-    assert.deepEqual(bigram.prediction('working'), ['with']);
-    assert.deepEqual(bigram.prediction('with'), ['node']);
-    assert.deepEqual(bigram.prediction('node'), []);
+    ngram.clean();
+    ngram.n = 2;
+    ngram.learn('I love working with node');
+    assert.deepEqual(ngram.prediction('I'), ['love']);
+    assert.deepEqual(ngram.prediction('love'), ['working']);
+    assert.deepEqual(ngram.prediction('working'), ['with']);
+    assert.deepEqual(ngram.prediction('with'), ['node']);
+    assert.deepEqual(ngram.prediction('node'), []);
   });
 
   it('should return two words if two prediction was learned', function () {
-    bigram.learn('I love you');
-    assert.deepEqual(bigram.prediction('love'), ['working', 'you']);
+    ngram.clean();
+    ngram.n = 2;
+    ngram.learn('I love working with node');
+    ngram.learn('I love you');
+    assert.deepEqual(ngram.prediction('love'), ['working', 'you']);
   });
 
   it('should not learn the same combination twice', function () {
-    bigram.learn('she like cats');
-    bigram.learn('she like cats');
-    bigram.learn('she like cats');
+    ngram.clean();
+    ngram.n = 2;
+    ngram.learn('she like cats');
+    ngram.learn('she like cats');
+    ngram.learn('she like cats');
 
-    assert.deepEqual(bigram.prediction('she'), ['like']);
+    assert.deepEqual(ngram.prediction('she'), ['like']);
   });
 
   it('should ignore upper and lower case', function () {
-    bigram.learn('animals are awesome');
-    assert.deepEqual(bigram.prediction('ANimALs'), ['are']);
+    ngram.clean();
+    ngram.n = 2;
+    ngram.learn('animals are awesome');
+    assert.deepEqual(ngram.prediction('ANimALs'), ['are']);
   });
 
   it('shoud use the last word, if the prediction got a sentence', function () {
-    bigram.learn('she like cats');
-    assert.deepEqual(bigram.prediction('she like'), ['cats']);
+    ngram.clean();
+    ngram.n = 2;
+    ngram.learn('she like cats');
+    assert.deepEqual(ngram.prediction('she like'), ['cats']);
   });
 
-  it('should work as treegram', function() {
-    var treegram = require('../lib');
-    treegram.n = 3;
-    treegram.learn('JavaScript is easy');
-    treegram.learn('CoffeeScript is simple');
-    assert.deepEqual(treegram.prediction('JavaScript is'), ['easy']);
+  it('should work as trigram', function () {
+    ngram.clean();
+    ngram.n = 3;
+    ngram.learn('JavaScript is easy');
+    ngram.learn('CoffeeScript is simple');
+    assert.deepEqual(ngram.prediction('JavaScript is'), ['easy']);
+  });
+
+  it('should work letter by letter', function () {
+    ngram.clean();
+    ngram.n = 3;
+    ngram.seperator = /[A-Z]/ig;
+    ngram.predictOnlyNext = false;
+    ngram.learn('JavaScript');
+    ngram.learn('Java');
+    ngram.learn('James');
+    assert.deepEqual(ngram.prediction('Ja'), ['vaScript', 'va', 'mes']);
+    assert.deepEqual(ngram.prediction('Jav'), ['aScript', 'a']);
   });
 });
